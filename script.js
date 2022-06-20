da11ae1284b412c89918a8319785eb184f0a65ba
Taby <forwardpivot@proton.me>
2022-06-20 12:33:48 -0500

    So, even though it says Person.prototype.getInfo it didn't work until I used Object.create. Super fun

--- script.js
+++ script.js
@@ -8,17 +8,24 @@ Person.prototype.getInfo = function() {
     my job is ${this.job}`);
 }
 
-function Hero(alterEgo, hideout) {
+function Hero(name, age, job, alterEgo, hideout) {
     this.name = name;
     this.age = age;
     this.job = job;
     this.alterEgo = alterEgo;
     this.hideout = hideout;
+    this.secretIdentity = function() {
+        console.log(`${alterEgo} is in the ${hideout}`);
+    }
 }
 
 Hero.prototype = Object.create(Person.prototype);
+Person.prototype = Object.create(Person.prototype);
 
 const bruce = new Hero("Bruce Wayne", 30, "Industrialist", "Batman", "Batcave");
+const vicki = new Person("Vicki Vale", 30, "Journalist");
 
 bruce.getInfo();
+bruce.secretIdentity();
+vicki.getInfo();
 
